09b74a4bf256c243400bd571ffdc15d1880136b1
hermes3306 <joonho.park@hotmail.com>
2018-10-30 03:27:54 +0000

    crontable

---
+++ addcron.sh
@@ -0,0 +1,16 @@
+echo "---"
+echo "-"
+echo "- add following line in the crontab"
+echo "-"
+echo "---"
+echo ""
+echo ""
+echo ""
+echo "0 * * * * /home/pi/code/MBox/cr0.sh"
+echo ""
+echo ""
+echo ""
+
+sleep 3
+
+crontab -e 
---
+++ cr0.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export mbox_home=`pwd`
+php src/cr0.php
