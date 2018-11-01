f59f78ebd6b27b5d648ebd1ccfb9719fb7c4e024
hermes3306 <joonho.park@hotmail.com>
2018-11-01 01:37:51 +0000

    Thu Nov  1 01:37:50 UTC 2018

---
+++ gitadd.sh
@@ -0,0 +1,6 @@
+git add *
+git config --global user.email "joonho.park@hotmail.com"
+git config --global user.name "hermes3306"
+git commit  -m "`date`"
+git push
+
---
+++ gitconf.sh
@@ -0,0 +1,3 @@
+git config --global user.email "joonho.park@hotmail.com"
+git config --global user.name "hermes3306"
+
