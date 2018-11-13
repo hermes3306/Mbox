d85fdb4fa3ee6b3ab1decadb6a033b9f0f9fd334
hermes3306 <joonho.park@hotmail.com>
2018-11-14 00:26:55 +0900

    x

---
+++ ggcaladd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#export mbox_home=`pwd`
+#export mbox_home=/Users/pi/code/MBox
+export mbox_home=/home/pi/code/MBox
+cd $mbox_home
+#php /Users/pi/code/MBox/src/ggcaladd.php
+php /home/pi/code/MBox/src/ggcaladd.php
--- ggd.sh
+++ ggd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export mbox_home=`pwd`
-cd  /home/jhpark/code/MBox
-php /home/jhpark/code/MBox/src/ggd.php
+cd  /home/pi/code/MBox
+php /home/pi/code/MBox/src/ggd.php
