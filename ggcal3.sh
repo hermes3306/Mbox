749e4b5d46dfa6d5873fbfa13875d0ca59a6b065
hermes3306 <joonho.park@hotmail.com>
2018-11-14 21:05:37 +0900

    x

--- ggcal3.sh
+++ ggcal3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #export mbox_home=`pwd`
-export mbox_home=/Users/pi/code/MBox
+#export mbox_home=/Users/pi/code/MBox
+export mbox_home=/home/pi/code/MBox
 cd $mbox_home
+#php /Users/pi/code/MBox/src/ggcal3.php
 php /Users/pi/code/MBox/src/ggcal3.php
